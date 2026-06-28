618cbefcbb39ceec429404d72969dc2f5f8fc8b8
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-06-28 21:50:17 +0530

    feat: added autostart feature

--- autostart.py
+++ autostart.py
@@ -10,31 +10,29 @@ def enable_autostart():
     else:
         exe_path = str(Path(__file__).parent / "dist" / "organizer.exe")
 
-    key = winreg.OpenKey(
+    with winreg.OpenKey(
         winreg.HKEY_CURRENT_USER,
         r"Software\Microsoft\Windows\CurrentVersion\Run",
         0,
         winreg.KEY_SET_VALUE
-    )
-    winreg.SetValueEx(key, APP_NAME, 0, winreg.REG_SZ, exe_path)
-    winreg.CloseKey(key)
-    print(f"Autostart enabled: {exe_path}")
+    ) as key:
+        winreg.SetValueEx(key, APP_NAME, 0, winreg.REG_SZ, exe_path)
+    print(f"Autostart Enabled: {exe_path}")
+
 
 
 def disable_autostart():
-    key = winreg.OpenKey(
+    with winreg.OpenKey(
         winreg.HKEY_CURRENT_USER,
         r"Software\Microsoft\Windows\CurrentVersion\Run",
         0,
         winreg.KEY_SET_VALUE
-    )
-    try:
-        winreg.DeleteValue(key, APP_NAME)
-        print("Autostart disabled.")
-    except FileNotFoundError:
-        print("Autostart was not enabled.")
-    winreg.CloseKey(key)
-
+    ) as key:
+        try:
+            winreg.DeleteValue(key, APP_NAME)
+            print("Autostart Disabled")
+        except FileNotFoundError:
+            print("Autostart Did Not Got Disabled")
 
 if __name__ == "__main__":
     enable_autostart()
